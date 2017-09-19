23c17b0cfd9a3e4cce694286ab5b1288be3948a3
sumi <sumnima9@gmail.com>
2017-09-19 20:20:58 +0100

    Initial commit

---
+++ TicTacToe/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TicTacToe
+{
+
+    public enum CrossDots
+    {
+        X = 1,
+        O = 2
+    }
+
+    public class Grid
+    {
+        private CrossDots[,] _grid;
+
+        public Grid()
+        {
+            _grid = new CrossDots[3, 3];
+        }
+
+        public bool GridIsEmpty()
+        {
+            foreach (CrossDots val in _grid)
+            {
+                if (val != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public CrossDots GetValueAt(int x, int y)
+        {
+            return _grid[x, y];
+        }
+
+        public void SetValueAt(int x, int y, CrossDots value)
+        {
+            _grid[x, y] = value;
+        }
+    }
+}
---
+++ TicTacToe/Test.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+namespace TicTacToe
+{
+    [TestFixture()]
+    public class Test
+    {
+        private Grid _grid;
+
+        [SetUp]
+        public void init()
+        {
+            _grid = new Grid();
+        }
+
+        [Test]
+        public void checkThatInitialGridIsEmpty()
+        {
+            Assert.True(_grid.GridIsEmpty());
+        }
+    }
+}
